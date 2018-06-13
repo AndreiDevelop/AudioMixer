ad524a9dfbc06eb0cbf9bb01fb75aa0d4ceb36e0
Andrei Maksimov <making714@gmail.com>
2018-06-13 17:33:04 +0300

    Add ability get audio clip from path url

---
+++ Assets/Scripts/AudioClipFromPath.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioClipFromPath : MonoBehaviour 
+{
+	private const string FILE_URL = "file:///";
+
+	[SerializeField] private string _path = string.Empty;
+	[SerializeField] private AudioSource _source = null;
+
+	[SerializeField] private string _url = string.Empty;
+
+	#if UNITY_EDITOR
+	private void OnValidate()
+	{
+		_url = FILE_URL + _path;
+	}
+	#endif
+
+    void Start()
+	{
+        _source = GetComponent<AudioSource>();
+        
+		StartCoroutine (GetAudioSourseWithDelay ());
+    }
+
+	private IEnumerator GetAudioSourseWithDelay()
+	{
+		Debug.Log ("1");
+		using (var www = new WWW(_url))
+		{
+			yield return www;
+			Debug.Log ("2");
+			_source.clip = www.GetAudioClip();
+		}
+
+		yield return new WaitUntil (()=>_source.clip != null && _source.clip.isReadyToPlay);
+		Debug.Log ("3");
+		_source.Play();
+	}
+}
--- Assets/Scripts/AudioMixer.cs
+++ Assets/Scripts/AudioMixer.cs
@@ -2,22 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using UnityEditor;
+using System.IO;
 
 public class AudioMixer : MonoBehaviour 
 {
-	private const float MIN = -1.0f;
-	private const float MAX = 1.0f;
+	private const float MIN_FLOAT = -1.0f;
+	private const float MAX_FLOAT = 1.0f;
 
 	[SerializeField]private AudioClip _firstAudio = null;
 	[SerializeField]private AudioClip _secondAudio = null;
 
+	[Space]
+	[SerializeField]private string _pathToFirstAudio = null;
+	[SerializeField]private string _pathToSecondAudio = null;
+
 	// Use this for initialization
 	void Start () 
 	{
-		MixAudio ();
+		MixAudioFloatExample ();
 	}
-	
-	private void MixAudio()
+
+	#if UNITY_EDITOR
+	private void OnValidate()
+	{
+		_pathToFirstAudio = AssetDatabase.GetAssetPath (_firstAudio);
+		_pathToSecondAudio = AssetDatabase.GetAssetPath (_secondAudio);
+	}
+	#endif
+
+	#region File in float format
+
+	private void MixAudioFloatExample()
 	{
 		float[] firstAudioInFloatFormat = new float[_firstAudio.samples * _firstAudio.channels];
 		_firstAudio.GetData(firstAudioInFloatFormat, 0);
@@ -34,20 +50,23 @@ public class AudioMixer : MonoBehaviour
 		SavWav.Save ("MixedClip", mixedClip);
 	}
 
-	private float ClampToValidRange(float value)
+	private float ClampToValidRangeFloat(float value)
 	{
-		return (value < MIN) ? MIN : (value > MAX) ? MAX : value;
+		return (value < MIN_FLOAT) ? MIN_FLOAT : (value > MAX_FLOAT) ? MAX_FLOAT : value;
 	}
 
 	private float[] MixAndClampFloatBuffers(float[] bufferA, float[] bufferB)
 	{
-		int maxLength = Math.Min(bufferA.Length, bufferB.Length);
-		float[] mixedFloatArray = new float[maxLength];
+		int length = Math.Min(bufferA.Length, bufferB.Length);
 
-		for (int i = 0; i < maxLength; i++)
+		float[] mixedFloatArray = new float[length];
+
+		for (int i = 0; i < length; i++)
 		{
-			mixedFloatArray[i] = ClampToValidRange((bufferA[i] + bufferB[i])/2);
+			mixedFloatArray[i] = ClampToValidRangeFloat((bufferA[i] + bufferB[i])/2);
 		}
 		return mixedFloatArray;
 	}
+
+	#endregion
 }
